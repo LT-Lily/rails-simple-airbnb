20334647252ad489d8c3d2eac8182877c5d427b6
Lily Tan <lilyt2995@gmail.com>
2020-09-13 12:46:45 +1000

    ARCH: frontend setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Cleaning database"
+Flat.destroy_all
+
+puts "Creating flats..."
+
+5.times do
+    flat = Flat.new(
+        name: Faker::Address.street_name,
+        address: Faker::Address.full_address,
+        description: Faker::Restaurant.description ,
+        price_per_night: 350,
+        number_of_guests: rand(1..6)
+    )
+    flat.save!
+end
+
+puts "Created #{Flat.count} flat(s)"
